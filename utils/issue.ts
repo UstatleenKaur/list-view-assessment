d3be46dfb1abd8c534d7d9f6f146bf932abb86d1
Ustatleen <ust.kaur@gmail.com>
2023-12-08 00:54:23 +0530

    feat(group-issue): added grouping logic (#2)

--- utils/issue.ts
+++ utils/issue.ts
@@ -1,3 +1,47 @@
+import { Issue, Label } from "@/contexts/issues";
+
 export function getInitials(name: string) {
   return name.split(" ").map(i => i.substring(0, 1)).join("").toUpperCase();
+}
+
+export function groupBy(records: Issue[], key?: keyof Issue) {
+  if (!key) return records;
+  return records.reduce((result: Record<any, Issue[]>, obj: Issue) => {
+    const keyValue: any = obj[key];
+
+    if (key === "labels") {
+      (keyValue as Label[]).forEach((label) => {
+        // Check if the key exists in the result object
+        if (!result[label]) {
+          result[label] = [];
+        }
+
+        // Push the current object to the corresponding key in the result object
+        result[label].push(obj);
+      });
+    } else if (key === "assignee") {
+      let currentKey = "unassigned";
+      if (keyValue) {
+        currentKey = "assigned";
+      }
+
+      if (!result[currentKey]) {
+        result[currentKey] = [];
+      }
+
+      // Push the current object to the corresponding key in the result object
+      result[currentKey].push(obj);
+
+    } else {
+      // Check if the key exists in the result object
+      if (!result[keyValue]) {
+        result[keyValue] = [];
+      }
+
+      // Push the current object to the corresponding key in the result object
+      result[keyValue].push(obj);
+    }
+
+    return result;
+  }, {});
 }
